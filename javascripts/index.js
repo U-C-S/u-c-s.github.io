8bf346f05b975546ae4b5424d7ce4ee1c7e45dff
U Chanakya S <50218121+devUCS@users.noreply.github.com>
2020-05-05 18:12:56 +0530

    v1.2.2

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,2 +1,8 @@
 console.log("Current Version - v1.2.R-0505a");
 console.log("Credits: Font-Awesome v5.13.0");
+
+function egg() {
+  var name = prompt("Hey, What's your name?");
+   if(name === null) { alert('You legendary idiot!'); }
+   else{ alert('Hey ' + name + ', You have been EGGed'); }
+}

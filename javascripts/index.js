9e4d25f20714e45dec24d229cc158ee1f7217eb1
U Chanakya S <50218121+devUCS@users.noreply.github.com>
2020-05-06 15:33:06 +0530

    v1.2.3
    R-0605a
    
    - Renamed the Egg to DEATH_BOT.
    - Improved the DEATH_BOT

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,8 +1,18 @@
 console.log("Current Version - v1.2.R-0505a");
 console.log("Credits: Font-Awesome v5.13.0");
 
-function egg() {
-  var name = prompt("Hey, What's your name?");
-   if(name === null) { alert('You legendary idiot!'); }
-   else{ alert('Hey ' + name + ', You have been EGGed'); }
+
+
+//The botty Function
+function BOTTy() {
+  if (screen.width < 600) { alert('I HATE MOBILES'); }
+  var name = prompt("Hey, What's your name? \nDon't type nothing");
+   var letters = name.length;
+   var notnum = isNaN(name);
+
+    if(name === null) { alert('YOU LEGENDARY IDIOT!'); }
+    else if (name === 'nothing') { alert('You are basically NOTHING!!!'); }
+    else if (notnum === false) { alert('Type a name, YOU FOOL'); }
+    else if (letters < 4) { alert('I dont believe your name is "' + name + '"'); }
+    else if (notnum === true) { alert('So ' + name + ', You will have a Bright Future'); }
 }

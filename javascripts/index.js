fb0cf3a34074879959a9801beda950cee4d02128
U Chanakya S <50218121+devUCS@users.noreply.github.com>
2020-05-06 16:00:35 +0530

    v1.2.03.3

--- javascripts/index.js
+++ javascripts/index.js
@@ -3,10 +3,10 @@ console.log("Credits: Font-Awesome v5.13.0");
 
 
 
-//The botty Function
+//The death_botty Function
 function BOTTy() {
-  var name = prompt("Hey, What's your name? \nDon't type nothing");
-   var letters = name.length;
+  var name = prompt("Hoi, What's your name? \nDon't type nothing");
+   if(name != null) {var letters = name.length; }
    var notnum = isNaN(name);
 
     if(name === null) { alert('YOU LEGENDARY IDIOT!'); }
@@ -14,4 +14,8 @@ function BOTTy() {
     else if (notnum === false) { alert('Type a name, YOU FOOL'); }
     else if (letters < 4) { alert('I dont believe your name is "' + name + '"'); }
     else if (notnum === true) { alert('So ' + name + ', You will have a Bright Future'); }
+
+
+
+ console.log("I'm DEATH_BOTTy, v1.3 created by @The-UCS-Variable")
 }

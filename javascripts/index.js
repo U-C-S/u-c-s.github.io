d8cf01b268b93bfc37716296cbd37af2338c8fe6
U Chanakya S <uchanakyasrinivas@gmail.com>
2020-05-17 14:43:28 +0530

    MAiN v1.3
    Simplified the Blog by removing useless content.

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,21 +1,2 @@
 console.log("Current Version - v1.2.R-0505a");
 console.log("Credits: Font-Awesome v5.13.0");
-
-
-
-//The death_botty Function
-function BOTTy() {
-  var name = prompt("Hoi, What's your name? \nDon't type nothing");
-   if(name != null) {var letters = name.length; }
-   var notnum = isNaN(name);
-
-    if(name === null) { alert('YOU LEGENDARY IDIOT!'); }
-    else if (name === 'nothing') { alert('You are basically NOTHING!!!'); }
-    else if (notnum === false) { alert('Type a name, YOU FOOL'); }
-    else if (letters < 4) { alert('I dont believe your name is "' + name + '"'); }
-    else if (notnum === true) { alert('So ' + name + ', You will have a Bright Future'); }
-
-
-
- console.log("I'm The BOcT, v1.1 created by @The-UCS-Variable")
-}

3893d5b5e72dcd3720d5529f43ea59e3d766839c
U Chanakya S <uchanakyasrinivas@gmail.com>
2020-06-10 15:25:35 +0530

    v1.3.25

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,1 +1,5 @@
 console.log("Credits: Font-Awesome v5.13.0");
+
+document.getElementById("navbtn").addEventListener('click', ()=>{
+    document.location = "https://the-ucs-variable.github.io/The-BOcT/"
+});

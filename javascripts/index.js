ccbb5a1e80997e7e6900a63cddfe5d49da01f48b
U Chanakya S <uchanakyasrinivas@gmail.com>
2020-06-12 15:16:20 +0530

    v1.3.41
    - Removed Font-Awesome online dependency

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,5 +1,7 @@
-console.log("Credits: Font-Awesome v5.13.0");
-
 document.getElementById("navbtn").addEventListener('click', ()=>{
     document.location = "https://the-ucs-variable.github.io/The-BOcT/"
+});
+
+document.getElementById("down-btn").addEventListener('click', ()=>{
+    document.location = "#content"
 });

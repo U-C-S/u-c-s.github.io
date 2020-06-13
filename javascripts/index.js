db51c95135be6c4b60251de622276ac1ba2ce0e5
U Chanakya S <uchanakyasrinivas@gmail.com>
2020-06-13 14:40:00 +0530

    v1.3.5
    - Theme color changes
    - Checkout btn now won't create extra link

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,7 +2,7 @@ document.getElementById("navbtn").addEventListener('click', ()=>{
     document.location = "https://the-ucs-variable.github.io/The-BOcT/"
 });
 document.getElementById("down-btn").addEventListener('click', ()=>{
-    document.location = "#content"
+    window.scrollTo(0,document.body.scrollHeight);
 });
 
 

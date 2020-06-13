705600112afa08be56614a79415297e0baeb9db3
U Chanakya S <uchanakyasrinivas@gmail.com>
2020-06-13 13:19:08 +0530

    v1.3.46
    - Changed main font to 'Kalam'
    - And some little Tweaks....

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,7 +1,14 @@
 document.getElementById("navbtn").addEventListener('click', ()=>{
     document.location = "https://the-ucs-variable.github.io/The-BOcT/"
 });
-
 document.getElementById("down-btn").addEventListener('click', ()=>{
     document.location = "#content"
 });
+
+
+
+/* DUMP 
+
+<i class="fas fa-hand-point-down"></i>
+
+*/

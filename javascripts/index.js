2358cc074dd21a582caef84fb0d73d98a4925113
U Chanakya S <uchanakyasrinivas@gmail.com>
2020-06-17 17:30:54 +0530

    v1.3.60
    - Few Tweaks here and there

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,14 +1,8 @@
+//current modification version: 1.3.60
+
 document.getElementById("navbtn").addEventListener('click', ()=>{
     document.location = "https://the-ucs-variable.github.io/The-BOcT/"
 });
 document.getElementById("down-btn").addEventListener('click', ()=>{
     window.scrollTo(0,document.body.scrollHeight);
 });
-
-
-
-/* DUMP 
-
-<i class="fas fa-hand-point-down"></i>
-
-*/

8d6f12c1dc45b1302c6ea00bbecd5be32225a499
U Chanakya S <uchanakyasrinivas@gmail.com>
2020-07-15 16:49:56 +0530

    v1.3.71

--- files/index.js
+++ files/index.js
@@ -1,6 +1,7 @@
-//current modification version: 1.3.7
+//current modification version: 1.3.71
+console.log("UPCOMING!!! v1.4");
 
 document.getElementById("navbtn").addEventListener('click', ()=>{
-    document.location = "https://the-ucs-variable.github.io/The-BOcT/"});
+    document.location = "https://the-boct.github.io/"});
 document.getElementById("down-btn").addEventListener('click', ()=>{
     window.scrollTo(0,document.body.scrollHeight);});

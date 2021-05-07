e7e9f7bde8b7c8920cbc3627ce2ab3a2b0bc2061
U-C-S <ucs3721@outlook.com>
2021-05-07 16:59:42 +0530

    Some Best Practices for Lighthouse 100 Score - v2-alpha-1.7

--- files/index.js
+++ files/index.js
@@ -26,5 +26,6 @@ colors: #39ff14 , #ff1439 , hsl(51, 100%, 59%)
 To-do list:
 - Add a Hover Tips popup at a fixed position
 - Ability for users to change the accent color (Maybe)
+- check out lit / web components stuff
 
 */

bc178f2d998dd58b0cae372e419b9150c1648669
U-C-S <ucs3721@outlook.com>
2021-06-14 16:37:03 +0530

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -4,30 +4,35 @@ console.log("version: 2.0.0-alpha");
     const AboutTabs = document.getElementsByClassName("abouttab");
     const AboutContent = document.getElementById("main-content");
     const ACTIVE_TAB = "activetab";
+    let def_Tab;
+    let param_Tab;
+    let thisPage = new URL(window.location.toString());
+    let tabParam = thisPage.searchParams.get("tab");
     for (let i = 0; i < AboutTabs.length; i++) {
         const tab = AboutTabs[i];
-        const templ = document.getElementById(`templ-${tab.dataset.tempid}`);
+        let temp_data = tab.dataset.tempid;
+        const templ = document.getElementById(`templ-${temp_data}`);
         tab.addEventListener("click", () => {
+            var _a;
             if (!tab.classList.contains(ACTIVE_TAB)) {
-                change_active_tab_color(tab);
+                (_a = document.getElementsByClassName(ACTIVE_TAB)[0]) === null || _a === void 0 ? void 0 : _a.classList.remove(ACTIVE_TAB);
+                tab.classList.add(ACTIVE_TAB);
                 let x = templ.content.cloneNode(true);
                 AboutContent.innerHTML = "";
                 AboutContent.appendChild(x);
+                thisPage.searchParams.set("tab", temp_data);
+                window.history.pushState({}, "", thisPage.toString());
             }
         });
-        if (tab.dataset.defopen) {
-            tab.click();
+        if (tabParam == temp_data) {
+            param_Tab = tab;
+        }
+        else if (tab.dataset.defopen) {
+            def_Tab = tab;
         }
     }
-    function change_active_tab_color(tab) {
-        var _a;
-        (_a = document.getElementsByClassName(ACTIVE_TAB)[0]) === null || _a === void 0 ? void 0 : _a.classList.remove(ACTIVE_TAB);
-        tab.classList.add(ACTIVE_TAB);
-    }
+    if (param_Tab)
+        param_Tab.click();
+    else
+        def_Tab === null || def_Tab === void 0 ? void 0 : def_Tab.click();
 })();
-const x = document.getElementById("The-Pro-Button");
-const thisPage = new URL(window.location.toString());
-x === null || x === void 0 ? void 0 : x.addEventListener("click", () => {
-    thisPage.searchParams.set("pro", "1");
-    window.history.pushState({}, "", thisPage.toString());
-});

4a89658aa607fc44e7bc812fc4229817c781d59e
Chanakya <ucs3721@outlook.com>
2021-07-01 13:26:48 +0530

    Revert "Layout Updates - v2-alpha-3"
    This reverts commit 7dcd5c8916243a201dca45f709c9d41ec896e4cd.

--- src/exports.ts
+++ src/exports.ts
@@ -1,15 +1,6 @@
-export function RandomNumber() {
+function RandomNumber() {
   let x = Math.random() + 1;
   return x;
 }
 
-export function OnOff(Id: string, spanId: string) {
-  document.getElementById(Id)!.onclick = () => {
-    let x = document.getElementById(spanId);
-    if (x!.innerHTML == "OFF") x!.innerText = "ON";
-    else x!.innerText = "OFF";
-  };
-}
-
-// Create a class for the element
-
+export default RandomNumber;

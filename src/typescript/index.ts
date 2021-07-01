b9dd7614280512c4cbfa6298991d24e655a3dd51
Chanakya <ucs3721@outlook.com>
2021-07-01 13:26:48 +0530

    Revert "Few minor updates - testing minify HTML again - use of IIFE - add other links in footer of index - add a article - who am i? (temp name) - add theme color meta tag"
    This reverts commit 79216c2d873f6e4b0ba9362c054fbc816936df34.

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -8,40 +8,29 @@ To-do list:
 - check out lit / web components stuff
 */
 
-(async () => {
-  const AboutTabs = document.getElementsByClassName("abouttab");
-  const AboutContent = <HTMLDivElement>document.getElementById("main-content");
-  const ACTIVE_TAB = "activetab";
+const AboutTabs = document.getElementsByClassName("abouttab");
+const AboutContent = <HTMLDivElement>document.getElementById("main-content");
+const ACTIVE_TAB = "activetab";
 
-  for (let i = 0; i < AboutTabs.length; i++) {
-    const tab = <HTMLButtonElement>AboutTabs[i];
-    const templ = <HTMLTemplateElement>document.getElementById(`templ-${tab.dataset.tempid}`);
+for (let i = 0; i < AboutTabs.length; i++) {
+  const tab = <HTMLButtonElement>AboutTabs[i];
+  const templ = <HTMLTemplateElement>document.getElementById(`templ-${tab.dataset.tempid}`);
 
-    tab.addEventListener("click", () => {
-      if (!tab.classList.contains(ACTIVE_TAB)) {
-        change_active_tab_color(tab);
-        let x = templ.content.cloneNode(true);
-        AboutContent.innerHTML = "";
-        AboutContent.appendChild(x);
-      }
-    });
-
-    if (tab.dataset.defopen) {
-      tab.click();
+  tab.addEventListener("click", () => {
+    if (!tab.classList.contains(ACTIVE_TAB)) {
+      change_active_tab_color(tab);
+      let x = templ.content.cloneNode(true);
+      AboutContent.innerHTML = "";
+      AboutContent.appendChild(x);
     }
+  });
+
+  if (tab.dataset.defopen) {
+    tab.click();
   }
+}
 
-  function change_active_tab_color(tab: HTMLButtonElement) {
-    document.getElementsByClassName(ACTIVE_TAB)[0]?.classList.remove(ACTIVE_TAB);
-    tab.classList.add(ACTIVE_TAB);
-  }
-})();
-
-// (async () => {
-//   const username = "u-c-s";
-//   let github_api_url = new URL(`https://api.github.com/users/${username}/events/public`);
-//   github_api_url.searchParams.append("per_page", "5");
-
-//   let fetchRes = await fetch(github_api_url.toString());
-//   let ResponseJson = await fetchRes.json();
-// })();
+function change_active_tab_color(tab: HTMLButtonElement) {
+  document.getElementsByClassName(ACTIVE_TAB)[0]?.classList.remove(ACTIVE_TAB);
+  tab.classList.add(ACTIVE_TAB);
+}

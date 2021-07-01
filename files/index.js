4a2904ffdbf424d50817ee4a036ae546b3e30c42
Chanakya <ucs3721@outlook.com>
2021-07-01 13:26:48 +0530

    Revert "alpha-1.6 - Added Dark mode btn - remove unused CSS"
    This reverts commit 9e0cd7074504f5c529a83737ad479869bb92cd04.

--- files/index.js
+++ files/index.js
@@ -1,4 +1,5 @@
-console.log("version: 2.0.0-alpha");
+console.log("version: 1.5.3");
+/* colors: #39ff14 , #ff1439 , hsl(51, 100%, 59%) */
 
 fetch("files/svg-paths.json")
   .then((res) => res.json())
@@ -9,20 +10,7 @@ fetch("files/svg-paths.json")
     }
   });
 
-function OnOff(Id, spanId) {
-  document.getElementById(Id).onclick = function () {
-    let x = document.getElementById(spanId);
-    if (x.innerHTML == "OFF") x.innerText = "ON";
-    else x.innerText = "OFF";
-  };
-}
-
-OnOff("The-Pro-Button", "Pro-status");
-OnOff("The-Theme-Button", "Theme-status");
-
 /*
-colors: #39ff14 , #ff1439 , hsl(51, 100%, 59%)
-
 To-do list:
 - Add a Hover Tips popup at a fixed position
 - Ability for users to change the accent color (Maybe)

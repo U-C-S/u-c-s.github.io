4ea7b3b40755834e1462f58df09813a3cac3c80d
Chanakya <ucs3721@outlook.com>
2021-07-01 13:26:48 +0530

    Revert "Updates like.... - Add logo as background image - add main border in large screens - remove "Open Sans" which I accidentally added"
    This reverts commit b66e4e474a9d0b604442efc3d9deb7856813913c.

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,4 +1,4 @@
-console.log("version: 2.0.0-beta");
+console.log("version: 2.0.0-alpha");
 /*
 colors: #39ff14 , #ff1439 , hsl(51, 100%, 59%)
 
@@ -6,12 +6,11 @@ To-do list:
 - Add a Hover Tips popup at a fixed position
 - Ability for users to change the accent color (Maybe)
 - check out lit / web components stuff
-- Check for fonts - at max 2 imported
 */
 
 /**
  * Simple API for updating the current URL with search queries
- *
+ * 
  * Uses BrowserAPIs URL: API and History API's replaceState method
  */
 const URLparams = {
@@ -20,7 +19,7 @@ const URLparams = {
   add: function (key: string, value: string) {
     let { url } = this;
     url.searchParams.set(key, value);
-    window.history.replaceState({}, document.title, url.toString());
+    window.history.replaceState({}, document.title, url.toString()); //see more about this method params
   },
 
   get: function (key: string) {

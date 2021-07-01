c68fd3301845d824e20c5290e8e4d7d7ac30aba1
Chanakya <ucs3721@outlook.com>
2021-07-01 13:26:48 +0530

    Revert "EOD commit - testing url params without page reload - increase main card size by 20% - Now, starting develop will open indexpage in browser"
    This reverts commit 3234cf15cafbdd1630be33b4f256b38585b863f1.

--- bin/develop.js
+++ bin/develop.js
@@ -5,6 +5,6 @@ console.log("Getting the tools ready for local development...");
 let developCmds = [
   { command: "tsc --watch --preserveWatchOutput", name: "tsc", prefixColor: "blue" },
   { command: "sass --watch --no-source-map src/sass:static/styles", name: "sass", prefixColor: "magenta" },
-  { command: "sleep 4 && zola serve --port 7321 --open", name: "zola", prefixColor: "white" },
+  { command: "sleep 4 && zola serve --port 7321", name: "zola", prefixColor: "white" },
 ];
 concurrently(developCmds);

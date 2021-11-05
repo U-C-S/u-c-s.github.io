bfb3c761266762a144d9c480e61419ede035dc47
Chanakya <ucs3721@outlook.com>
2021-11-05 17:32:27 +0530

    simplify solid components code - removed usage of createMemo

--- src/typescript/components/gh-events.tsx
+++ src/typescript/components/gh-events.tsx
@@ -1,17 +1,16 @@
-import { createMemo, createResource, For } from "solid-js";
+import { createResource, For } from "solid-js";
 import { render } from "solid-js/web";
 import { EventParse } from "./gh-event-parser";
 
-const [eventsAPI] = createResource<ghEventApi[]>(async () => {
-  let res = await fetch(`https://api.github.com/users/U-C-S/events/public?per_page=5`);
-  return res.json();
-});
-
 const App = () => {
-  const status = createMemo(() => eventsAPI());
+  const [eventsAPI] = createResource<ghEventApi[]>(async () => {
+    let res = await fetch(`https://api.github.com/users/U-C-S/events/public?per_page=5`);
+    return res.json();
+  });
+
   return (
     <>
-      <For each={status()} fallback={<h2>Getting Github Events....</h2>}>
+      <For each={eventsAPI()} fallback={<h2>Getting Github Events....</h2>}>
         {(item) => <li>{EventParse(item)}</li>}
       </For>
     </>
--- src/typescript/components/status.tsx
+++ src/typescript/components/status.tsx
@@ -1,4 +1,4 @@
-import { createMemo, createResource, Show } from "solid-js";
+import { createResource, Show } from "solid-js";
 import { render } from "solid-js/web";
 
 interface IComment {
@@ -7,24 +7,26 @@ interface IComment {
   body: string;
 }
 
-const [commentsAPI] = createResource<IComment[]>(async () => {
-  let res = await fetch(`https://api.github.com/repos/U-C-S/u-c-s.github.io/issues/10/comments`);
-  return res.json();
-});
-
 const App = () => {
-  const status = createMemo(() => commentsAPI()?.pop());
+  const [commentsAPI] = createResource<IComment[]>(async () => {
+    let res = await fetch(`https://api.github.com/repos/U-C-S/u-c-s.github.io/issues/10/comments`);
+    return res.json();
+  });
 
   return (
     <>
-      <Show when={status()} fallback={<p>Getting Status...</p>}>
-        <>
-          <p class="status-content">{status()?.body}</p>
-          <div class="status-meta">
-            <a href={status()?.html_url}>Dynamic_Status</a>
-            <p>Updated On: {status()?.created_at.substring(0, 10)}</p>
-          </div>
-        </>
+      <Show when={commentsAPI()?.pop()} fallback={<p>Getting Status...</p>}>
+        {(status) => {
+          return (
+            <>
+              <p class="status-content">{status.body}</p>
+              <div class="status-meta">
+                <a href={status.html_url}>Dynamic_Status</a>
+                <p>Updated On: {status.created_at.substring(0, 10)}</p>
+              </div>
+            </>
+          );
+        }}
       </Show>
     </>
   );
